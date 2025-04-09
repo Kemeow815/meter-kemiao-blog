07ecb478e1a5c3c4d7802903e4b7bfdbd5084214
Kemeow815 <kemiao@kmblog.icu>
2025-04-09 09:44:59 +0800

    Update site info and links

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "StudyingLover's Blog"; // site title
-export const SITE_TAGLINE = 'StudyingLover'; // site tagline
-export const SITE_DESCRIPTION = 'Work hard in secret, then amaze everyone.'; // site meta description for SEO
+export const SITE_TITLE = "Ke Miao's Blog"; // site title
+export const SITE_TAGLINE = 'Ke Miao'; // site tagline
+export const SITE_DESCRIPTION = '每一段旅行都有终点~'; // site meta description for SEO
